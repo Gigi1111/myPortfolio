8fb21c0544aff9e7a214c08cb37ab222c59c5460
Gigi1111 <airolg1111@gmail.com>
2019-09-23 04:41:59 +0800

    Updates

---
+++ precache-manifest.c315561cf574268e6fe421c82ff50fae.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05fefb52dde832cb5348e8ea9e60e9cd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d812bda9140d32e629e3",
+    "url": "/portfolio/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "8d80a89be3594680f315",
+    "url": "/portfolio/static/css/main.caea95c1.chunk.css"
+  },
+  {
+    "revision": "d812bda9140d32e629e3",
+    "url": "/portfolio/static/js/2.1a26d31c.chunk.js"
+  },
+  {
+    "revision": "8d80a89be3594680f315",
+    "url": "/portfolio/static/js/main.e02dd4eb.chunk.js"
+  },
+  {
+    "revision": "87374000eec951793c83",
+    "url": "/portfolio/static/js/runtime~main.0abbd2dd.js"
+  },
+  {
+    "revision": "e3f6e15a3ceb2cbac166e3047d985e85",
+    "url": "/portfolio/static/media/1down.e3f6e15a.png"
+  },
+  {
+    "revision": "e8746e49344b8c908e684608f1a2d6af",
+    "url": "/portfolio/static/media/2down.e8746e49.png"
+  },
+  {
+    "revision": "44902859782df5701a0d158eca13c823",
+    "url": "/portfolio/static/media/3down.44902859.png"
+  },
+  {
+    "revision": "7d8becbb312cdd297dfd55b3ff7e1de2",
+    "url": "/portfolio/static/media/arrow.7d8becbb.png"
+  },
+  {
+    "revision": "1044ebeb8eafdc157d7e14f713ff9e31",
+    "url": "/portfolio/static/media/education.1044ebeb.png"
+  },
+  {
+    "revision": "6262f64b719a3adba54a520f24ebb505",
+    "url": "/portfolio/static/media/experience.6262f64b.png"
+  },
+  {
+    "revision": "11b202e5faa05d212e97d4353708c7f2",
+    "url": "/portfolio/static/media/me.11b202e5.jpg"
+  },
+  {
+    "revision": "8fd1924b501e3bf3e452d9b4e43bb1e2",
+    "url": "/portfolio/static/media/skills.8fd1924b.png"
+  },
+  {
+    "revision": "c5588753ac2fba69747e298b48a8c8b2",
+    "url": "/portfolio/static/media/tape1.c5588753.png"
+  },
+  {
+    "revision": "fae854551b5c9b34bf8a080405c849b9",
+    "url": "/portfolio/static/media/tape2.fae85455.png"
+  },
+  {
+    "revision": "57683ed894291d572495c8ce7a5f3670",
+    "url": "/portfolio/static/media/up1.57683ed8.png"
+  },
+  {
+    "revision": "8fc581fd331826782b3a584f2e40395b",
+    "url": "/portfolio/static/media/up2.8fc581fd.png"
+  },
+  {
+    "revision": "87f0b5d3b0f034273b5be82610eb07c2",
+    "url": "/portfolio/static/media/up3.87f0b5d3.png"
+  },
+  {
+    "revision": "edc5dc17b245f07fabac278c823e6655",
+    "url": "/portfolio/static/media/zipper.edc5dc17.png"
+  }
+]);
